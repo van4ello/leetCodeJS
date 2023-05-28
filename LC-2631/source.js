b15ad4a492cdfbf9130fa71f9302942ad6acea98
van4ello <van4ello@gmail.com>
2023-05-28 13:49:23 +0200

    LC-2631:ADD: 2631. Group By. 4 solutions added

---
+++ LC-2631/source.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Function} fn
+ * @return {Array}
+ */
+Array.prototype.groupBy = function(fn) {
+  const group = {};
+  for (const element of this) {
+    const key = fn(element);
+    if (!(key in group)) group[key] = [];
+    group[key].push(element);
+  }
+  return group; 
+};
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
